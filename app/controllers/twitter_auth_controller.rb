c1f25ccb5381277df84b36eddda5d627998b06a5
Cezar Halmagean <cezar@mixandgo.com>
2023-03-08 09:46:15 +0200

    Updated notice

--- app/controllers/twitter_auth_controller.rb
+++ app/controllers/twitter_auth_controller.rb
@@ -2,6 +2,6 @@ class TwitterAuthController < ApplicationController
   def create
     token = request.env.dig("omniauth.auth", :credentials, :token)
     current_user.update(token: token)
-    redirect_to root_path, notice: "Tweeter account connected."
+    redirect_to root_path, notice: "Twitter account connected."
   end
 end
